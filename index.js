fbda968d9a51b983ff1b5fd870840cf46b14c7b5
JR <jadedResearcher@gmail.com>
2022-12-25 10:55:39 -0500

    that backrooms buzz

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 //https://codepen.io/chris22smith/pen/PbBwjp  source for frame css
-
+let number_clicks = 0;
 window.onload = ()=>{
   window.alert("JR SAYS HI");
   kickoffImageRenderingToCanvas("images/Hallways/00019-img.png",document.querySelector("#canas_viewport"));
+}
+
+window.onclick = () => {
+  const audio = document.querySelector("#audio");
+  if (!audio.playing) {
+    audio.play();
+  }
+  number_clicks++;
 }
