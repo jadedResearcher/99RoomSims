e9c97aaecd184016a518527eaa163b1f0e9a4dd7
JR <jadedResearcher@gmail.com>
2022-12-25 11:19:28 -0500

    i think i have done crimes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //https://codepen.io/chris22smith/pen/PbBwjp  source for frame css
 let number_clicks = 0;
 window.onload = ()=>{
-  window.alert("JR SAYS HI");
   kickoffImageRenderingToCanvas("images/Hallways/00019-img.png",document.querySelector("#canas_viewport"));
 }
 
