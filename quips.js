9ead0e1008b29ae7d24314cca691c715fde7748e
JR <jadedResearcher@gmail.com>
2022-12-25 22:01:04 -0500

    i'm restrainging myself to only a single secret

--- quips.js
+++ quips.js
@@ -10,8 +10,23 @@ What is a door, really.
 Are you sure that was a door?
 What was that?`
 
-const genericQuips  = genericQuipsRaw.split("\n")
+const genericHallwayQuipsRaw = 
+`How far do you think these hallways go?
+Ever forward, that's how you make progress. Right?`
 
-getQuipFor = (imageKey)=>{
+const genericRoomQuipsRaw = 
+`Have you been in this room before?
+Was this always a room?
+Wasn't...wasn't this a hallway before?`
+
+const genericQuips  = genericQuipsRaw.split("\n")
+const genericRoomQuips  = genericRoomQuipsRaw.split("\n")
+const genericHallwayQuips  = genericHallwayQuipsRaw.split("\n")
+
+
+getQuipFor = (imageKey,currently_room)=>{
+  if(Math.random() > 0.5){
+    return currently_room? pickFrom(genericRoomQuips) : pickFrom(genericHallwayQuips);
+  }
   return pickFrom(genericQuips);
 }
