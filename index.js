bf0bae1f64f96992ae57ea938a2e09e35310e285
JR <jadedResearcher@gmail.com>
2023-01-08 22:45:57 -0500

    tipping system

--- index.js
+++ index.js
@@ -21,12 +21,17 @@ restaurant_name
 : 
 "Master  Fries" */
 let details;
+let tip = 0.0;
 window.onload = () => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   let tmp_ref = urlParams.get('referer');
   let tmp_d = urlParams.get('details');
+  let tmp_tip = urlParams.get('tip');
 
+  if(tmp_tip){
+    tip = tmp_tip;
+  }
   let tmp_ref_details = urlParams.get('referer_details') &&urlParams.get('referer_details').replaceAll('"','');
   if (tmp_ref) {
     referer = tmp_ref;
@@ -55,7 +60,8 @@ const getReferrerDetailsQuips = ()=>{
   const feeUnder = details.feeUnder;
   const restaurant_name = details.restaurant_name;
 
-  return ["","","","","","","",
+  let tmp = ["","","","","","","",
+    
     `Personally, I'm not sure I'd have risked all this for a lousy ${item_name}.`
     ,`$${price} seems really cheap for a ${item_name}, but they get you with the fees.`
     ,`Just couldn't bring yourself to spend $${feeUnder} to avoid the fee of wandering this maze, could you?`
@@ -65,6 +71,27 @@ const getReferrerDetailsQuips = ()=>{
 
     ,`Maybe next time you can just pay the $${feeUnder} and avoid all this.`
   ]
+  if(tip == 0){
+    tmp.push("Maybe you should have tipped?");
+    tmp.push("Man, bet that driver didn't like how you didn't tip.");
+    tmp.push("The fee's what doomed you and all but I bet the lack of a tip didn't help...");
+
+  }else if(tip < 10){
+    tmp.push(`Did you really think $${tip} was enough?`);
+    tmp.push(`Would YOU have driven all that way for a lousy $${tip}?`);
+    tmp.push(`Not a great tipper, huh?`);
+  }else if (tip > 100){
+    tmp.push(`Wow, you woulda thought that for a $${tip} tip the driver at least wouldn't have sent you to the maze.`);
+    tmp.push(`Man, you could just used that  $${tip} tip to pay your fee. `);
+    tmp.push(`What on earth compelled you to tip  $${tip}? `);
+
+  }else{
+    tmp.push(`$${tip} tip? You've worked in the service industry, haven't you?`);
+    tmp.push(`$${tip} tip? Nice.`);
+    tmp.push(`$${tip} tip? Pretty decent of you.`);
+
+  }
+  return tmp;
 }
 
 const getReferrerQuips = ()=>{
